e98c8d3c115a0976d5a64e4f0870ef695bb0f78c
sleijon1 <simonleijon59@gmail.com>
2022-12-03 10:16:28 +0100

    d3

---
+++ day3/day3.py
@@ -0,0 +1,33 @@
+from aocutils.clients.client import submit, fetch
+
+fetch()
+
+rucksacks = open("day3/input.txt").read().splitlines()
+
+priorities_1, priorities_2 = 0, 0
+last_three = []
+
+def calc_prio(in_common):
+    priorities = 0
+    for char in in_common:
+        if char.isupper():
+            priorities += ord(char) - 38
+            continue
+        priorities += ord(char) - 96
+    return priorities
+
+for i, rucksack in enumerate(rucksacks):
+    j = i+1
+    half = len(rucksack)//2
+    in_common = set(rucksack[:half]).intersection(set(rucksack[half:]))
+    priorities_1 += calc_prio(in_common)
+    last_three.append(rucksack)
+    if j % 3 == 0:
+        in_common_2 = set(last_three[0]).intersection(set(last_three[1]), set(last_three[2]))
+        priorities_2 += calc_prio(in_common_2)
+        last_three = []
+
+print(priorities_1)        
+print(priorities_2)
+# submit(priorities_1)
+# submit(priorities_2)
