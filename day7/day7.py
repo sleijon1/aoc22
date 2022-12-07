277582da2aa667e2343e63a8f62940d3e3409b9b
sleijon1 <simonleijon59@gmail.com>
2022-12-07 18:57:04 +0100

    cleaned

--- day7/day7.py
+++ day7/day7.py
@@ -2,50 +2,44 @@ from aocutils.clients.client import fetch, submit
 
 fetch()
 
-cmds = open("day7/input.txt").read().splitlines()
+cmds = open("day7/input.txt").read().splitlines()[::-1]
 
-stack, sizes = [], {}
-popped = []
-i = 0
-# print(cmds)
-end = False
-while i < len(cmds):
-    # print(cmds[i], i)
-    if end or "cd .." in cmds[i]:
+stack, popped = [], []
+end, i = False, 0
+cmd = cmds.pop()
+
+while cmds:
+    if "cd .." in cmd:
         dir_name, subdirs = stack.pop()
         popped.append((dir_name, subdirs))
-        i += 1
-    elif "cd" in cmds[i] and ".." not in cmds[i]:
-        _, _, dir_name = cmds[i].split(" ")
+        cmd = cmds.pop()
+    elif "cd" in cmd and ".." not in cmd:
+        _, _, dir_name = cmd.split(" ")
         stack.append([dir_name, []])
-        i += 1
-    elif "ls" in cmds[i]:
-        i += 1
-        while "$" not in cmds[i]:
-            if "dir" in cmds[i]:
-                i += 1
+        cmd = cmds.pop()
+    elif "ls" in cmd:
+        cmd = cmds.pop()
+        while "$" not in cmd:
+            if "dir" in cmd:
+                cmd = cmds.pop()
                 continue
-            size, file_name = cmds[i].split(" ")
+            size, file_name = cmd.split(" ")
             for prev_dir in stack:
                 prev_dir[1].append((size, file_name))
-            i += 1
-            if i == len(cmds):
-                end = True
-                i -= 1
+            if not cmds:
                 break
+            cmd = cmds.pop()
 
 final = popped + stack
 total_sum_100 = 0
 sizes = []
 for dir_name, sub_dirs in final:
-    print(dir_name, sub_dirs)
     sub_dir_sum = sum([int(size) for (size, _) in sub_dirs])
     if sub_dir_sum < 100000:
         total_sum_100 += sub_dir_sum
     sizes.append(sub_dir_sum)
 total_sum = sum([int(size) for size, dir_name in stack[0][1]])
-unused = 70000000 - total_sum
-need = 30000000 - unused
+need = 30000000 - (70000000 - total_sum)
 candidates = [size for size in sizes if size >= need]
 print(total_sum_100, min(candidates))
 # submit(total_sum_100)
