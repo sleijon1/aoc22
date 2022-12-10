7b6d90f2434cf7d96195dcf8ccf732b08e698a2c
sleijon1 <simonleijon59@gmail.com>
2022-12-10 07:51:39 +0100

    clean

--- day10/day10.py
+++ day10/day10.py
@@ -4,47 +4,38 @@ fetch()
 
 ops = open("day10/input.txt").read().splitlines()
 
-X, last_val = 1, 0
+X, last_val, cycle, i = 1, 0, 1, 0
 segments = []
-cycle, i = 1, 0
 crt = [['.' for _ in range(40)] for _ in range(6)]
+
+
+def run_cycle():
+    global X, cycle
+
+    row = (cycle-1) // 40
+    column = (cycle-1) % 40
+    if column in (X-1, X, X+1):
+        crt[row][column] = '#'
+    cycle += 1
+
+
 while i < len(ops):
     op = ops[i]
     if "add" in op:
         last_val = int(op.split(" ")[1])
         for k in range(2):
-            row = (cycle-1) // 40
-            column = (cycle-1) % 40
-            if column in (X-1, X, X+1):
-                draw = '#'
-            else:
-                draw = '.'
-            print(draw, row, column, X)
-            cycle += 1
-            crt[row][column] = draw
+            run_cycle()
             if k == 1:
                 X += last_val
             if cycle == 20 or (20+cycle) % 40 == 0:
                 segments.append(X*cycle)
     else:
-        row = (cycle-1) // 40
-        column = (cycle-1) % 40
-        if column in (X-1, X, X+1):
-            draw = '#'
-        else:
-            draw = '.'
-        cycle += 1
-        crt[row][column] = draw
+        run_cycle()
         if (cycle == 20 or (20+cycle) % 40 == 0):
             segments.append(X*(cycle))
-
     i += 1
 
-
-print(X)
-print(segments)
 print(sum(segments))
 for row in crt:
     print(''.join(row))
-#submit(answer=sum(segments))
-submit(answer="RGZEHURK", level=2)
+# submit(answer="RGZEHURK", level=2)
