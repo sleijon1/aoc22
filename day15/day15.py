c74a05f0815a7ab35b7f1f896939ba4b062206e1
Simon <simonleijon59@gmail.com>
2022-12-17 12:20:21 +0100

    d15 done, horrible performance

--- day15/day15.py
+++ day15/day15.py
@@ -27,24 +27,46 @@ def manhattan(p1, p2):
     left_y, right_y = min(s_y, b_y), max(s_y, b_y)
     mh_dist = abs(left_x - right_x) + abs(left_y - right_y)
     return mh_dist
-row = 2000000
+
+rows = defaultdict(list)
 for sensor in sensors:
     s_x, s_y, b_x, b_y = map(lambda x: int(x.strip("=")), re.findall('=[-]?[0-9]+', sensor))
-    print("SENSOR", (s_x, s_y))
+    #print("SENSOR", (s_x, s_y))
     points[(s_x, s_y)] = "S"
     points[(b_x, b_y)] = "B"
     mh_dist = manhattan((s_x, s_y), (b_x, b_y))
-    x_range = range(s_x-mh_dist, s_x+mh_dist+1)
-    y_range = range(s_y-mh_dist, s_y+mh_dist+1)
-    if row in y_range:
-        y_range = [row]
-        for new_x in x_range:
-            for new_y in y_range:
-                if manhattan((s_x, s_y), (new_x, new_y)) <= mh_dist:
-                    points[(new_x, new_y)] = "#"
+    y_top = s_y+mh_dist+1
+    y_bot = s_y-mh_dist
+    # this will include perimeter
+    for i, j in enumerate(range(y_top, s_y-1, -1)):
+        rows[j].append([s_x-i, s_x+i])
+    for i, j in enumerate(range(y_bot-1, s_y)):
+        rows[j].append([s_x-i, s_x+i])
+
+print("have we done this mate?")
+bounds = 4000000
+for y in range(bounds+1):
+    ranges = rows[y]
+    print(y)
+    beacon_point = None
+    for i in range(len(ranges)):
+        for perimeter_point in ranges[i]:
+            if not (bounds >= perimeter_point >= 0):
+                continue
+            possible_beacon = True
+            for j in range(len(ranges)):
+                if i != j:
+                    if perimeter_point in range(ranges[j][0]+1, ranges[j][1]):
+                        possible_beacon = False
+                        break
+            if possible_beacon:
+                beacon_point = (perimeter_point, y)
+                break
+        if possible_beacon:
+            print("BEACON MIGHT BE HERE MATE", beacon_point)
+            print(f"Tuning frequency {beacon_point[0]*4000000+y}")
+            exit()
+
+        
 
 
-points_on_row = [point for point, value in points.items() 
-                 if value not in ("B", ".") and point[1] == row]
-cant_be_beacon = len(points_on_row)
-print(cant_be_beacon)
