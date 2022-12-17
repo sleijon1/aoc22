f3db0a7420843e4f2d059571a976386c46ac1a4a
Simon <simonleijon59@gmail.com>
2022-12-17 15:36:45 +0100

    added back d15a, 1 min run time..

--- day15/day15.py
+++ day15/day15.py
@@ -1,23 +1,10 @@
 from collections import defaultdict
 import re
+from time import time
 
+start = time()
 sensors = open("input.txt").read().splitlines()
 points = defaultdict(lambda: ".")
-
-def calc_overlap(square_1, square_2):
-    overlap_x = min(square_1[0]) > min(square_2[0]) and max(square_2[0]) > max(square_1[0])\
-                or min(square_2[0]) > min(square_1[0]) and max(square_1[0]) > max(square_2[0])
-    overlap_y = min(square_1[1]) > min(square_2[1]) and max(square_2[1]) > max(square_1[1])\
-                or min(square_2[1]) > min(square_1[1]) and max(square_1[1]) > max(square_2[1])
-    if overlap_x and overlap_y:
-        left_x, right_x = min(square_1[0], square_2[0]), min(square_1[0], square_2[0])
-        left_y, right_y = min(square_1[1], square_2[1]), min(square_1[1], square_2[1])
-    return
-
-
-def reduce_ranges(ranges):
-    pass
-
 
 
 def manhattan(p1, p2):
@@ -28,10 +15,10 @@ def manhattan(p1, p2):
     mh_dist = abs(left_x - right_x) + abs(left_y - right_y)
     return mh_dist
 
+row = 2000000
 rows = defaultdict(list)
 for sensor in sensors:
     s_x, s_y, b_x, b_y = map(lambda x: int(x.strip("=")), re.findall('=[-]?[0-9]+', sensor))
-    #print("SENSOR", (s_x, s_y))
     points[(s_x, s_y)] = "S"
     points[(b_x, b_y)] = "B"
     mh_dist = manhattan((s_x, s_y), (b_x, b_y))
@@ -43,30 +30,41 @@ for sensor in sensors:
     for i, j in enumerate(range(y_bot-1, s_y)):
         rows[j].append([s_x-i, s_x+i])
 
-print("have we done this mate?")
-bounds = 4000000
-for y in range(bounds+1):
-    ranges = rows[y]
-    print(y)
-    beacon_point = None
-    for i in range(len(ranges)):
-        for perimeter_point in ranges[i]:
-            if not (bounds >= perimeter_point >= 0):
-                continue
-            possible_beacon = True
-            for j in range(len(ranges)):
-                if i != j:
-                    if perimeter_point in range(ranges[j][0]+1, ranges[j][1]):
-                        possible_beacon = False
-                        break
-            if possible_beacon:
-                beacon_point = (perimeter_point, y)
-                break
-        if possible_beacon:
-            print("BEACON MIGHT BE HERE MATE", beacon_point)
-            print(f"Tuning frequency {beacon_point[0]*4000000+y}")
-            exit()
+    if row in range(y_bot, y_top):
+        y_range = [row]
+        for new_x in range(s_x-mh_dist, s_x+mh_dist+1):
+            for new_y in y_range:
+                if manhattan((s_x, s_y), (new_x, new_y)) <= mh_dist:
+                    points[(new_x, new_y)] = "#"
 
-        
+
+def find_beacon():
+    bounds = 4000000
+    for y in range(bounds+1):
+        ranges = rows[y]
+        beacon_point = None
+        for i in range(len(ranges)):
+            for perimeter_point in ranges[i]:
+                if not (bounds >= perimeter_point >= 0):
+                    continue
+                possible_beacon = True
+                for j in range(len(ranges)):
+                    if i != j:
+                        if perimeter_point in range(ranges[j][0]+1, ranges[j][1]):
+                            possible_beacon = False
+                            break
+                if possible_beacon:
+                    beacon_point = (perimeter_point, y)
+                    break
+            if possible_beacon:
+                return beacon_point[0]*4000000+y
+
+
+points_on_row = [point for point, value in points.items() 
+                 if value not in ("B", ".") and point[1] == row]
+
+print(f"Positions cannot contain beacon: {len(points_on_row)}")
+print(f"Tuning frequency {find_beacon()}")
+print(f"Took {time()-start} seconds")
 
 
